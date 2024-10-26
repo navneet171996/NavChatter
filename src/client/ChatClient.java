03d9e3bdbf87b27566dc03530162b48555b825cf
navneet171996 <dutta.navneet17@gmail.com>
2024-10-26 20:40:22 +0530

    Initial connection and message sending done from client to server

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,4 +1,75 @@
 package client;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
 public class ChatClient {
+    private PrintWriter writer;
+    private JTextField outgoingMessage;
+
+    public void startClient(){
+        setupConnection();
+
+        outgoingMessage = new JTextField(20);
+
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(a -> sendMessage());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(outgoingMessage);
+        mainPanel.add(sendButton);
+
+        JFrame mainFrame = new JFrame("NavChatter");
+        mainFrame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        mainFrame.setSize(500, 300);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+    }
+    private void setupConnection(){
+        try {
+            SocketAddress socketAddress = new InetSocketAddress("127.0.0.1", 5000);
+            SocketChannel channel = SocketChannel.open(socketAddress);
+
+            writer = new PrintWriter(Channels.newWriter(channel, StandardCharsets.UTF_8));
+            System.out.println("Connection Established with Server at time: " + System.currentTimeMillis());
+        } catch (IOException e){
+            System.out.println("Error in connecting to the server");
+        }
+    }
+    public String read(){
+//        reader = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
+//        bufferedReader = new BufferedReader(reader);
+//        try {
+//            String message = bufferedReader.readLine();
+//            return message;
+//        } catch (IOException e) {
+//            return "ERROR reading message";
+//        }
+        return "";
+    }
+
+    public void write(String message){
+//        writer = Channels.newWriter(socketChannel, StandardCharsets.UTF_8);
+//        printWriter = new PrintWriter(writer);
+//        printWriter.println(message);
+    }
+
+    private void sendMessage(){
+        writer.println(outgoingMessage.getText());
+        writer.flush();
+        outgoingMessage.setText("");
+        outgoingMessage.requestFocus();
+    }
+
+    public static void main(String[] args){
+        new ChatClient().startClient();
+    }
 }
--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,4 +1,87 @@
 package server;
 
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ChatServer {
+
+    List<PrintWriter> clientWriters = new ArrayList<>();
+
+    public void startListening(){
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress(5000));
+
+            while(serverSocketChannel.isOpen()){
+                SocketChannel clientSocketChannel = serverSocketChannel.accept();
+                PrintWriter writer = new PrintWriter(Channels.newWriter(clientSocketChannel, StandardCharsets.UTF_8));
+                clientWriters.add(writer);
+                threadPool.submit(new ClientHandler(clientSocketChannel));
+                System.out.println("Successfully made a connection with the client");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void read(SocketChannel clientSocket){
+        String message;
+        BufferedReader reader = new BufferedReader(Channels.newReader(clientSocket, StandardCharsets.UTF_8));
+        try {
+            while((message = reader.readLine()) != null){
+                System.out.println(message);
+                write();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void write(){
+        for (PrintWriter writer:clientWriters){
+            writer.println("Message received");
+            writer.flush();
+        }
+    }
+
+    public static void main(String[] args){
+        ChatServer chatServer = new ChatServer();
+        chatServer.startListening();
+
+    }
+
+    public class ClientHandler implements Runnable{
+        BufferedReader reader;
+        SocketChannel channel;
+
+        public ClientHandler(SocketChannel socketChannel){
+            channel = socketChannel;
+            reader = new BufferedReader(Channels.newReader(channel, StandardCharsets.UTF_8));
+        }
+        @Override
+        public void run() {
+            String message;
+            try {
+                while((message = reader.readLine()) != null){
+                    System.out.println(message);
+                    write();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
